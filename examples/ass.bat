ec030c69caa813cb7aa28757291c80af6876be14
Wil <wilfried@gmx.at>
2020-04-22 23:05:34 +0200

    use delay_ms instead of wait

---
+++ examples/ass.bat
@@ -0,0 +1,11 @@
+rem Assembling file for C64 target
+@echo off
+>nul findstr /c:"makesys" %1 && (
+  @echo on
+  cl65 %1 -lib ../LAMAlib.lib -C c64-asm.cfg -o %~n1.prg
+) || (
+  @echo on
+  cl65 %1 -lib ../LAMAlib.lib -C c64-asm.cfg -u __EXEHDR__ -o %~n1.prg
+)
+
+
