3210c977429b0e39cd949e8180d73b91f0fec1f2
Wil <wilfried@gmx.at>
2021-04-04 20:54:41 +0200

    new name for documentation script

---
+++ update_documentation.bat
@@ -0,0 +1,13 @@
+@rem update_documentation
+@rem an MSDOS batch file (runs on Windows) to execute parsedoc with the parameters for the LAMAlib project
+@rem 
+@rem Usage: makedoc
+@rem python 3.x needs to be installed on your system and to be in your path
+@rem
+@rem Version: 0.2 
+@rem Date: 2021-04-04
+@rem Author: Wil Elmenreich (wilfried at gmx dot at)
+@rem License: The Unlicense (public domain)
+
+python parsedoc.py LAMAlib.inc > LAMAlibdoc.html
+pause
