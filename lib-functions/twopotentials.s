2f1239c3e4f672dc22cb958285f93b2acf3f6889
Wil <wilfried@gmx.at>
2021-06-25 23:39:57 +0200

    updated 1 files

---
+++ lib-functions/twopotentials.s
@@ -0,0 +1,7 @@
+; this module provides a table with the potentials of two
+; this table is often needed and therefore provided centrally here
+
+.export _twopotentials
+
+_twopotentials: .byte $01,$02,$04,$08,$10,$20,$40,$80
+
