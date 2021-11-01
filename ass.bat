f5cd9b506167f3f33df506e65d8a7d911475f74d
Wil <wilfried@gmx.at>
2021-11-01 12:15:38 +0100

    extended ass script to C128

--- ass.bat
+++ ass.bat
@@ -4,24 +4,49 @@
 
 @echo off
 
+set TARGET=c64
+
 if "%1"=="" (
   echo Usage: %0 asmfile [startaddr]
   echo Calls the cl65 assembler and linker and creates an executable .PRG for C64
   exit /b
 )
+if "%1"=="-128" (
+  set TARGET=c128
+  shift
+) else if "%1"=="-c128" (
+  set TARGET=c128
+  shift
+) else if "%1"=="-C128" (
+  set TARGET=c128
+  shift
+)
+
+if "%1"=="-64" (
+  set TARGET=c64
+  shift
+) else if "%1"=="-c64" (
+  set TARGET=c64
+  shift
+) else if "%1"=="-C64" (
+  set TARGET=c64
+  shift
+)
+
+
 if "%2"=="" (
-  echo assembling %1 for target C64...
+  echo assembling %1 for target %TARGET%...
   >nul findstr /c:"makesys" %1 && (
     @echo on
-    cl65 "%1" -lib LAMAlib.lib -C c64-basicfriendly-asm.cfg -Ln "labels.txt" -o "%~n1.prg"
+    cl65 "%1" -lib LAMAlib.lib -t %TARGET% -C %TARGET%-basicfriendly-asm.cfg -Ln "labels.txt" -o "%~n1.prg"
   ) || (
     @echo on
-    cl65 "%1" -lib LAMAlib.lib -C c64-basicfriendly-asm.cfg -Ln "labels.txt" -u __EXEHDR__ -o "%~n1.prg"
+    cl65 "%1" -lib LAMAlib.lib -t %TARGET% -C %TARGET%-basicfriendly-asm.cfg -Ln "labels.txt" -u __EXEHDR__ -o "%~n1.prg"
   )
 ) else (
-  echo assembling %1 to start address %2 for target C64...
+  echo assembling %1 to start address %2 for target %TARGET%...
   @echo on
-  cl65 "%1" -lib LAMAlib.lib -C c64-basicfriendly-asm.cfg -Ln "labels.txt" --start-addr %2 -o "%~n1.prg"
+  cl65 "%1" -lib LAMAlib.lib -t %TARGET% -C %TARGET%-basicfriendly-asm.cfg -Ln "labels.txt" --start-addr %2 -o "%~n1.prg"
 )
 @echo done.
 
