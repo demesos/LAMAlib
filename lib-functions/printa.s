9afdb223dd6b271b4b0da12702d9c3a812964c4a
demesos <wilfried.elmenreich@aau.at>
2022-04-09 21:28:43 +0200

    print function now preserves A,X,Y

---
+++ lib-functions/printa.s
@@ -0,0 +1,37 @@
+.include "LAMAlib.inc"
+
+.export _printa_sr
+
+; prints the 8bit number in A to the screen
+; code from codebase64.org
+
+_printa_sr:
+	ldy #$2f
+	ldx #$3a
+	sec
+:	iny
+	sbc #100
+	bcs :-
+:	dex
+	adc #10
+	bmi :-
+	adc #$2f
+
+	cpy #$30
+	if eq
+	  cpx #$31
+	  if ge
+	    pha
+	    txa
+	    jsr CHROUT
+	    pla
+	  endif	
+	else
+	  pha
+	  tya
+	  jsr CHROUT
+	  txa
+	  jsr CHROUT
+	  pla
+	endif
+	jmp CHROUT
