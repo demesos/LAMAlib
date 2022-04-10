7990e0989fe64cce2035809168e8a0c2f4eaf56c
demesos <wilfried.elmenreich@aau.at>
2022-04-11 01:25:10 +0200

    added table with masked two potentials

---
+++ lib-functions/maskedtwopotentials.s
@@ -0,0 +1,7 @@
+; this module provides a table with the potentials of two masked out
+; this table is often needed and therefore provided centrally here
+
+.export _maskedtwopotentials
+
+_maskedtwopotentials: .byte $FE,$FD,$FB,$F7,$EF,$DF,$BF,$7F
+
