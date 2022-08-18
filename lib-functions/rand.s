ffe844ea2d6672e67b6c7b2d462f9aa86cb2469e
demesos <wilfried.elmenreich@aau.at>
2022-08-18 19:13:46 +0200

    update random function implementation

--- lib-functions/rand.s
+++ lib-functions/rand.s
@@ -1,27 +1,35 @@
-;; X ABC Algorithm Random Number Generator for 8-Bit Devices
+;; 16-bit "798" Xorshift
 ;;
-;; Algorithm from EternityForest, slight modification by Wil
-;; https://www.electro-tech-online.com/threads/ultra-fast-pseudorandom-number-generator-for-8-bit.124249/
-;; This version stores the seed as arguments and uses self-modifying code
-;; Routine requires 50 cycles / 28 bytes
+;; Algorithm: George Marsaglia
+;; Implementation idea: John Metcalf
+;; Code: Veikko Sariola, Wil Elmenreich
+;;
+;; https://codebase64.org/doku.php?id=base:16bit_xorshift_random_generator
+;; Routine requires 46 cycles / 25 bytes
 
-.export _rand8_sr:=rand8, _rand_seed1:=x1, _rand_seed2:=a1, _rand_seed3:=b1, _rand_seed4:=c1
+.export _rand16_sr, _seed_low, _seed_high
 
-rand8:	
-	inc x1
-	clc
-x1=*+1
-	lda #$00	;x1
-c1=*+1
-	eor #$c2	;c1
-a1=*+1
-	eor #$11	;a1
-	sta a1
-b1=*+1
-	adc #$37	;b1
-	sta b1
-	lsr
-	eor a1
-	adc c1
-	sta c1
-	rts
+_rand16_sr: 
+_seed_high=*+1
+	lda #$33
+        lsr
+_seed_low=*+1
+        lda #$33
+        ror
+        eor _seed_high
+        sta _seed_high   ; high part of x ^= x << 7 done
+        ror             ; A has now x >> 9 and high bit comes from low byte
+        eor _seed_low
+        sta _seed_low    ; x ^= x >> 9 and the low part of x ^= x << 7 done
+        eor _seed_high 
+        sta _seed_high   ; x ^= x << 8 done
+        rts
+
+; set seed with value from AX
+; commented out because will be implemented as a macro
+;set_seed:
+;	cmp #1		;set carry if value != 0
+;	sbc #0
+;	sta _seed_low
+;	stx _seed_high
+;	rts
