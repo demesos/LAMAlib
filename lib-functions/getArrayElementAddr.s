14da46305fb4a654b025ca8126fdebf8abf208d7
Wil <wilfried.elmenreich@aau.at>
2023-01-18 01:12:33 +0100

    getkey and updated getAddrOfBasicArryVar

--- lib-functions/getArrayElementAddr.s
+++ lib-functions/getArrayElementAddr.s
@@ -1,12 +1,12 @@
 ; locates the descriptor for a variable specified at $46-$46
 ; before calling, the variable name and its flags need to be set in $45/$46
 
-.include "../LAMAlib-ROMfunctions.inc"
+;.include "../LAMAlib-ROMfunctions.inc"
 
 .export _getArrayElementAddr_sr
 
-.import _findarrayelement
-.importzp _var_flags
+.import FINDARRAYELEMENT
+.importzp VAR_FLAGS, CURR_VAR_NAME
 
 .proc _getArrayElementAddr_sr
 	;push array index
@@ -15,18 +15,18 @@
 	pha
 
 	ldy #00
-	sty _var_flags+1		;we don't have the DIM case
+	sty VAR_FLAGS+1		;we don't have the DIM case
 	lda CURR_VAR_NAME
 	and #$80
-	sta _var_flags+3		;$80 for integer vars, 0 otherwise
+	sta VAR_FLAGS+3		;$80 for integer vars, 0 otherwise
 	bmi skip
 	bit CURR_VAR_NAME+1
 	bpl skip
 	dey
 skip:
-	sty _var_flags+2		;0=numeric variable, ff=string
+	sty VAR_FLAGS+2		;0=numeric variable, ff=string
 	lda #1
-	sta _var_flags		;1 dimension
+	sta VAR_FLAGS		;1 dimension
 	
-	jmp _findarrayelement	;this ROM function searches for the variable and creates it if necessary
+	jmp FINDARRAYELEMENT	;this ROM function searches for the variable and creates it if necessary
 .endproc
