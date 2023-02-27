a4fd4adfd081c41c737cb848730558df7639a77b
Wil <wilfried.elmenreich@aau.at>
2023-02-27 23:34:49 +0100

    improved wait_screen_off

--- lib-functions/wait_screen_off.s
+++ lib-functions/wait_screen_off.s
@@ -1,15 +1,18 @@
-;waits until raster>255 and turns screen off
+;waits until raster>249 and turns screen off
 
 .export _wait_screen_off
 
 .proc _wait_screen_off
 	lda $D011
 	and #%01101111	;mask out raster high bit and screen on bit
+	ldx #249
 
-	;wait for raster > 255  
+	;wait for raster > 249  
 wtlp00: bit $D011
-	bpl wtlp00
-
+	bmi after_loop	;if this bit is set we have a rasterline>=256
+	cpx $D012	;check lower 8 bits of rasterline
+	bcs wtlp00	;loop if value<=249
+after_loop:
 	sta $D011	;turn screen off
 	rts
 .endproc
