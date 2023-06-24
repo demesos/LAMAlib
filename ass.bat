3c25855d013f514574c28c955f267d1b61063cfe
Wil <wilfried.elmenreich@aau.at>
2023-06-24 12:07:08 +0200

    added option for defining assembler makros with -d in ass.bat

--- ass.bat
+++ ass.bat
@@ -5,59 +5,61 @@
 @echo off
 
 set TARGET=c64
+set ASMDEF= 
 
 if "%1"=="" (
   echo Usage: %0 [-128^|20] asmfile [startaddr]
   echo Calls the cl65 assembler and linker and creates an executable .PRG for C64, unless -128 or -20 are specified, then result is for C128 or VIC20
   exit /b
 )
-if "%1"=="-128" (
+
+:check_options
+
+if "%1"=="-d" (
+  set ASMDEF=--asm-define %2
+  shift
+  shift
+  goto check_options
+) 
+
+if "%~1"=="-128" goto do128
+if /i "%~1" neq "-c128" goto not128
+:do128
   set TARGET=c128
   shift
-) else if "%1"=="-c128" (
-  set TARGET=c128
-  shift
-) else if "%1"=="-C128" (
-  set TARGET=c128
-  shift
-)
+  goto check_options
+:not128
 
-if "%1"=="-64" (
+if "%~1"=="-64" goto do64
+if /i "%~1" neq "-c64" goto not64
+:do64
   set TARGET=c64
   shift
-) else if "%1"=="-c64" (
-  set TARGET=c64
-  shift
-) else if "%1"=="-C64" (
-  set TARGET=c64
-  shift
-)
+  goto check_options
+:not64
 
-if "%1"=="-20" (
+if "%~1"=="-20" goto do20
+if /i "%~1"=="-vic20" goto do20
+if /i "%~1" neq "-vc20" goto not20
+:do20
   set TARGET=vic20
   shift
-) else if "%1"=="-vc20" (
-  set TARGET=vic20
-  shift
-) else if "%1"=="-vic20" (
-  set TARGET=vic20
-  shift
-)
-
+  goto check_options
+:not20
 
 if "%2"=="" (
   echo assembling %1 for target %TARGET%...
   >nul findstr /c:"makesys" %1 && (
     @echo on
-    cl65 -t %TARGET% "%1" -lib LAMAlib.lib -C %TARGET%-basicfriendly-asm.cfg -Ln "labels.txt" -o "%~n1.prg"
+    cl65 -t %TARGET% %ASMDEF% "%1" -lib LAMAlib.lib -C %TARGET%-basicfriendly-asm.cfg -Ln "labels.txt" -o "%~n1.prg"
   ) || (
     @echo on
-    cl65 -t %TARGET% "%1" -lib LAMAlib.lib -C %TARGET%-basicfriendly-asm.cfg -Ln "labels.txt" -u __EXEHDR__ -o "%~n1.prg"
+    cl65 -t %TARGET% %ASMDEF% "%1" -lib LAMAlib.lib -C %TARGET%-basicfriendly-asm.cfg -Ln "labels.txt" -u __EXEHDR__ -o "%~n1.prg"
   )
 ) else (
   echo assembling %1 to start address %2 for target %TARGET%...
   @echo on
-  cl65 -t %TARGET% "%1" -lib LAMAlib.lib -C %TARGET%-basicfriendly-asm.cfg -Ln "labels.txt" --start-addr %2 -o "%~n1.prg"
+  cl65 -t %TARGET% %ASMDEF% "%1" -lib LAMAlib.lib -C %TARGET%-basicfriendly-asm.cfg -Ln "labels.txt" --start-addr %2 -o "%~n1.prg"
 )
 @echo done.
 
