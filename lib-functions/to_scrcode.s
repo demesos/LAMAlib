29e2c37e9fde10d4a84ffe752e9214a954b31637
Wil <wilfried.elmenreich@aau.at>
2023-07-14 01:57:09 +0200

    to_scrcode

---
+++ lib-functions/to_scrcode.s
@@ -0,0 +1,20 @@
+.export to_scrcode_sr
+
+.proc to_scrcode_sr
+	cmp #$ff
+	bne L0
+	lda #126     ;pi character
+L0:
+        cmp #$60
+        bcc L1+1
+	cmp #$80
+	bcs L2
+	and #$df     ;delete bit $20 to handle uppercase chars
+L2:
+        ora #$40
+        and #$7f
+L1:
+        bit $3f29    ;contains command AND #$3f 
+	rts
+.endproc
+
