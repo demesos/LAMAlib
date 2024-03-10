7a161ef21f7599311d31da2a76ef7e664704e7c3
Wil <wilfried.elmenreich@aau.at>
2024-03-11 00:08:15 +0100

    added function for right aligned number printing

---
+++ lib-functions/print_fillchars_a.s
@@ -0,0 +1,36 @@
+.include "../LAMAlib-macros16.inc"
+.include "../LAMAlib-structured.inc"
+.include "../LAMAlib-ROMfunctions.inc"
+
+.export _print_fillchars_a_sr
+
+; prints the fillchars for a right-aligned decimal number
+; does not print the number itself
+; A number to print
+; Y fillchar
+; Function leaves A unchanged
+; code by Wil, 2024
+
+_print_fillchars_a_sr:
+	sty fillchar_value
+	ldy #0
+	cmp #10
+	if cc
+	  ldy #2
+	else
+	  cmpax #100
+	  if cc
+	    ldy #1
+	  endif
+	endif
+	pha
+fillchar_value=*+1
+	lda #32
+	jmp loop_entry
+fillchar_loop:
+	jsr CHROUT
+loop_entry:
+	dey
+	bpl fillchar_loop
+	pla
+	rts
---
+++ lib-functions/print_fillchars_ax.s
@@ -0,0 +1,46 @@
+.include "../LAMAlib-macros16.inc"
+.include "../LAMAlib-structured.inc"
+.include "../LAMAlib-ROMfunctions.inc"
+
+.export _print_fillchars_ax_sr
+
+; prints the fillchars for a right-aligned decimal number
+; does not print the number itself
+; AX number to print
+; Y fillchar
+; Function leaves AX unchanged
+; code by Wil, 2024
+
+_print_fillchars_ax_sr:
+	sty fillchar_value
+	ldy #0
+	cmpax #10
+	if cc
+	  ldy #4
+	else
+	  cmpax #100
+	  if cc
+	    ldy #3
+	  else
+	    cmpax #1000
+	    if cc
+	      ldy #2
+	    else
+              cmpax #10000
+              if cc
+                ldy #1
+              endif
+	    endif
+          endif
+	endif
+	pha
+fillchar_value=*+1
+	lda #32
+	jmp loop_entry
+fillchar_loop:
+	jsr CHROUT
+loop_entry:
+	dey
+	bpl fillchar_loop
+	pla
+	rts
