c642002f39e7196e849c69d90cd4de1ae070ed39
Wil <wilfried.elmenreich@aau.at>
2024-11-24 18:15:43 +0100

    AX option for passing filename in load prg, warp mode in test run

--- tests/run_test.bat
+++ tests/run_test.bat
@@ -3,7 +3,7 @@
 call ass testsuite.s
 @if not errorlevel 1 (
   echo starting testsuite...
-  start x64 -autostartprgmode 1 testsuite.prg
+  start x64 -warp -autostartprgmode 1 testsuite.prg
 ) else (
   pause
 )
